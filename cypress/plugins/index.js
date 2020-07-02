95672b1b798f89e56cdc2d39a8659f0babb30aa4
annalouisa17 <anna.phillips@onepeloton.com>
2020-07-02 13:34:13 -0400

    visual testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,10 +12,16 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+let percyHealthCheck = require('@percy/cypress/task')
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  initPlugin(on, config);
+  on("task", percyHealthCheck);
+  return config;
 }
